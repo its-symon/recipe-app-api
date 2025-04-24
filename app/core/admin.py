8fe36ccc290ac894551acd3a385ca7ce0765759a
sin1ter <its.symonbarua@gmail.com>
2025-04-24 17:43:41 +0600

    Documentation Setup

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core.models import User
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['id', 'email', 'name', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
