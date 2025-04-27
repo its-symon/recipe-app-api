f93658dc77976ea0078cda05ad610bdcd212921b
sin1ter <its.symonbarua@gmail.com>
2025-04-27 22:30:33 +0600

    Recipe API Done

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import User
+from core.models import User, Recipe
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
@@ -20,3 +20,4 @@ class UserAdmin(BaseUserAdmin):
     )
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Recipe)
